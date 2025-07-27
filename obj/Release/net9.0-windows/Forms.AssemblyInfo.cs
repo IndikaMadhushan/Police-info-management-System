00e718cbfdc8593bbe950a30483685f391f99fb1
IndikaMadhushan <indikamad.i@gmail.com>
2025-07-27 13:54:51 +0530

    creating first step of database connection

--- obj/Release/net9.0-windows/Forms.AssemblyInfo.cs
+++ obj/Release/net9.0-windows/Forms.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forms")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f85691d3b7d28abd918c04901ea0b76cd848033")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forms")]
