41266c3540062b6500821035bc66391f4ba37d4b
Jkr200210 <kavishanrandi@gmail.com>
2025-07-28 21:59:51 +0530

    payment form

--- obj/Debug/net9.0-windows/Forms.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c48340f7e537ef00350c642f1b6e1167f3de2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4962df8c4add112b2fa769a074be89ada19bff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
