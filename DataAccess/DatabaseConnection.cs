584a6f0b3bccf3fb4b3fad58c1b53fbc57383fe4
IndikaMadhushan <indikamad.i@gmail.com>
2025-10-07 12:14:09 +0530

    added singleton pattern to the database connection and modify the authentication and faul repository

--- DataAccess/DatabaseConnection.cs
+++ DataAccess/DatabaseConnection.cs
@@ -1,33 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient; // ✅ modern namespace
 
 namespace Forms.DataAccess
 {
-    public class DatabaseConnection
+    public sealed class DatabaseConnection
     {
-        private static string _connectionString;
-        
-        static DatabaseConnection()
+        private static readonly Lazy<DatabaseConnection> _instance =
+            new Lazy<DatabaseConnection>(() => new DatabaseConnection());
+
+        private readonly string _connectionString;
+
+        // Nullable connection for safety
+        private SqlConnection? _connection;
+
+        private DatabaseConnection()
         {
-            //Connectin String
-            _connectionString = @"Data Source=Indika\SQLEXPRESS;Initial Catalog=PoliceInfoManagementDB;Integrated Security=True;";
-        }
-        
-        public static SqlConnection getConnection()
-        {
-            SqlConnection conn = new SqlConnection(_connectionString);
-            return conn;
+            _connectionString = @"Data Source=Indika\SQLEXPRESS;Initial Catalog=PoliceInfoManagementDB;Integrated Security=True;TrustServerCertificate=True;";
+            
+
         }
 
-        public static bool TestConnectin()
+        public static DatabaseConnection Instance => _instance.Value;
+
+        public SqlConnection GetConnection()
+        {
+            // Always create a *new* connection for thread safety
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public bool TestConnection()
         {
             try
             {
-                using (var connection = getConnection())
+                using (var connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
                     return true;
